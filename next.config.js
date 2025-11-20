430e99963eb253f73cea4c5c75378c3551d05e94
omerhamdiu17-web <omer.hamdiu17@gmail.com>
2025-11-20 16:30:08 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+};
+
+module.exports = nextConfig;
